4f0b7ed7b85760c9d0e749ad855fcd6292aec562
Beedoc <meedoc@gmail.com>
2016-10-08 21:59:20 +0200

    added content

--- src/assets/twerk/pieces.lua
+++ src/assets/twerk/pieces.lua
@@ -1,28 +1,134 @@
 return {
-  Mediapart = {
-    name = "Mediapart",
-    pieceType = "PieceNewspaper",
-    connections = {
-      E = { type = "financial", convex = false },
-      W = { type = "world", convex = false }
+  -- Newpaper
+    Mediapart = {
+      name = "Mediapart",
+      team = "Player",
+      pieceType = "PieceNewspaper",
+      connections = {
+        E = { type = "financial", convex = false },
+        W = { type = "world", convex = false }
+      }
+    },
+    -- Journalists
+    Financial = {
+      name = "Financial",
+      team = "Player",
+      pieceType = "PieceJournalist",
+      connections = {
+        N = { type = "financial", convex = true }
+      }
+    },
+    World = {
+      name = "World",
+      team = "Player",
+      pieceType = "PieceJournalist",
+      connections = {
+        N = { type = "world", convex = true }
+      }
+    },
+    Sexual = {
+      name = "Sexual",
+      team = "Player",
+      pieceType = "PieceJournalist",
+      connections = {
+        N = { type = "sexual", convex = true }
+      }
+    },
+    State = {
+      name = "State",
+      team = "Player",
+      pieceType = "PieceJournalist",
+      connections = {
+        N = { type = "state", convex = true }
+      }
+    },
+    Joker=  {
+      name = "Joker",
+      team = "Player",
+      pieceType = "PieceJournalist",
+      connections = {
+        N = { type = "financial", convex = true },
+        S = { type = "world", convex = true },
+        E = { type = "sexual", convex = true },
+        W = { type = "state", convex = true }
+      }
+    },
+    -- Candidates
+    Reac = {
+      name = "Reac",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "financial", convex = false },
+        S = { type = "world", convex = false },
+        E = { type = "sexual", convex = false },
+        W = { type = "state", convex = false }
+      }
+    },
+    Socialo = {
+      name = "Socialo",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "financial", convex = false },
+        S = { type = "world", convex = false },
+        E = { type = "sexual", convex = false },
+        W = { type = "state", convex = false }
+      }
+    },
+    Facho = {
+      name = "Facho",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "financial", convex = false },
+        S = { type = "world", convex = false },
+        W = { type = "state", convex = false }
+      }
+    },
+    Gaucho = {
+      name = "Gaucho",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "financial", convex = false },
+        E = { type = "sexual", convex = false },
+        W = { type = "state", convex = false }
+      }
+    },
+    Centre = {
+      name = "Centre",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "financial", convex = false },
+        S = { type = "world", convex = false }
+      }
+    },
+    Ecolo = {
+      name = "Ecolo",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "sexual", convex = false },
+        S = { type = "state", convex = false }
+      }
+    },
+    Ailleurs = {
+      name = "Ailleurs",
+      team = "Enemy",
+      pieceType = "PieceCandidate",
+      connections = {
+        N = { type = "sexual", convex = false }
+      }
+    },
+    -- Adversary
+    Intelligence = {
+      name = "Intelligence",
+      team = "Enemy",
+      pieceType = "PieceAdversary",
+      connections = {
+        N = { type = "world", convex = true }
+      }
     }
-  },
-  Reac = {
-    name = "Reac",
-    pieceType = "PieceCandidate",
-    connections = {
-      N = { type = "financial", convex = true },
-      S = { type = "world", convex = true },
-      E = { type = "sexual", convex = true },
-      W = { type = "state", convex = true }
-    }
-  },
-  Ecolo = {
-    name = "Ecolo",
-    pieceType = "PieceCandidate",
-    connections = {
-      N = { type = "sexual", convex = true },
-      S = { type = "state", convex = true }
-    }
-  }
 }
--- src/assets/twerk/spawn.lua
+++ src/assets/twerk/spawn.lua
@@ -1,5 +1,65 @@
 return {
   {
+    grid = "newspaperGrid",
+    count = 1,
+    possiblePositions = {
+      { col = 1, row = 3 },
+      { col = 1, row = 5 },
+      { col = 1, row = 1 },
+      { col = 3, row = 1 },
+      { col = 3, row = 5 }
+    },
+    name = {"Sexual"}
+  },
+  {
+    grid = "newspaperGrid",
+    count = 1,
+    possiblePositions = {
+      { col = 1, row = 3 },
+      { col = 1, row = 5 },
+      { col = 1, row = 1 },
+      { col = 3, row = 1 },
+      { col = 3, row = 5 }
+    },
+    name = {"State"}
+  },
+  {
+    grid = "newspaperGrid",
+    count = 1,
+    possiblePositions = {
+      { col = 1, row = 3 },
+      { col = 1, row = 5 },
+      { col = 1, row = 1 },
+      { col = 3, row = 1 },
+      { col = 3, row = 5 }
+    },
+    name = {"World"}
+  },
+  {
+    grid = "newspaperGrid",
+    count = 1,
+    possiblePositions = {
+      { col = 1, row = 3 },
+      { col = 1, row = 5 },
+      { col = 1, row = 1 },
+      { col = 3, row = 1 },
+      { col = 3, row = 5 }
+    },
+    name = {"Financial"}
+  },
+  {
+    grid = "newspaperGrid",
+    count = 1,
+    possiblePositions = {
+      { col = 1, row = 3 },
+      { col = 1, row = 5 },
+      { col = 1, row = 1 },
+      { col = 3, row = 1 },
+      { col = 3, row = 5 }
+    },
+    name = {"Joker"}
+  },
+  {
     grid = "societyGrid",
     count = 1,
     possiblePositions = {
@@ -7,7 +67,7 @@ return {
       { col = 5, row = 1 },
       { col = 6, row = 1 }
     },
-    name = { "Mediapart" }
+    name = {"Mediapart"}
   },
   {
     grid = "societyGrid",
@@ -19,7 +79,7 @@ return {
       { col = 3, row = 4 },
       { col = 7, row = 4 }
     },
-    name = { "Reac" }
+    name = { "Facho", "Gaucho" }
   },
   {
     grid = "societyGrid",
@@ -31,6 +91,6 @@ return {
       { col = 3, row = 4 },
       { col = 7, row = 4 }
     },
-    name = { "Ecolo" }
+    name = { "Ecolo" , "Centre" }
   }
 }
