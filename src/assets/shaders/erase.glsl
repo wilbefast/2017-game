dcd8a176a882ad9f17f6538d58bbeb720749ded5
William Dyce <wilbefast@gmail.com>
2016-10-09 14:13:48 +0200

    cleaner concave cutout

--- src/assets/shaders/erase.glsl
+++ src/assets/shaders/erase.glsl
@@ -5,9 +5,6 @@ vec4 effect(vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords)
   vec4 result = Texel(texture, texture_coords);
   vec4 m = Texel(mask, texture_coords);
 
-  if(m.a > 0)
-  {
-    result.a = 0;
-  }
+  result.a *= 1 - m.a;
   return result;
 }
